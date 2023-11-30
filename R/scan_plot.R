7d115458b35d10875ea4dfdae7e58de7ec135bd6
Jaeseon Lee <jaeseon.lee@tamu.edu>
2023-11-30 15:37:15 -0600

    Specified the required package in scan_plot using ::.

--- R/scan_plot.R
+++ R/scan_plot.R
@@ -2,14 +2,17 @@
 #'
 #' Plot spatial hot-spot detected by scan statistics (Kulldorff, 1997).
 #'
+#' @import RColorBrewer
+#' @import grDevices
+#' @import graphics
+#'
 #' @param data N (space) X M (time) matrix
 #' @param shp shape file with identical space order with data.
-#' @param id list of cluster detected by scan.seq function
-#' @param wd working directory for saving plots
+#' @param id list of cluster. output of scan.seq function
+#'
+#' @export
 
-scan_plot <- function(data, shp, id, wd) {
-
-  library(RColorBrewer)
+scan_plot <- function(data, shp, id) {
 
   if (is.null(id)) {
     stop("id is null, so implement scan statistics using scan.seq function")
@@ -17,7 +20,6 @@ scan_plot <- function(data, shp, id, wd) {
 
   n.space <- nrow(shp)
 
-  setwd(wd)
   col1 <- c(); temp <-0
   for (i in 1:length(id)) {
 
@@ -26,16 +28,16 @@ scan_plot <- function(data, shp, id, wd) {
     counts <- data[,index]
     cluster2 <- id[[i]]$shp.order
 
-    jpeg(paste("cluster_",unique(id[[i]]$week),".jpeg",sep = ""), res=600, width=200, height=150, pointsize=9,units='mm')
+    grDevices::jpeg(paste("cluster_",unique(id[[i]]$week),".jpeg",sep = ""), res=600, width=200, height=150, pointsize=9,units='mm')
     brks <- cut(counts, breaks=c(0,210,490,1010,1600,max(counts)+1), include.lowest = TRUE, right=FALSE)
     cols <- RColorBrewer::brewer.pal(5, "Reds")
     for (k in 1:n.space) {col1[k] <- cols[brks[k]]}
     plot(shp$geometry, col=col1,axes=F, border="gray")
     plot(shp$geometry[cluster2], border="black", add=TRUE)
-    title(paste("Cluster map for", unique(id[[i]]$week), sep=" "))
-    legend('bottomright',legend=c("[0, 210)", "[210, 490)", "[490, 1010)", "[1010, 1600)", "≥ 1600"),fill=cols,
+    graphics::title(paste("Cluster map for", unique(id[[i]]$week), sep=" "))
+    graphics::legend('bottomright',legend=c("[0, 210)", "[210, 490)", "[490, 1010)", "[1010, 1600)", "≥ 1600"),fill=cols,
            cex=0.7)
-    dev.off()
+    grDevices::dev.off()
 
     print(temp)
   }
