a5b2bbf9054f9e921f576fcea2f7d93a257ef5b7
Jaeseon Lee <jaeseon.lee@tamu.edu>
2023-11-30 12:13:09 -0600

    Separated scan statistics function with plotting one.

--- R/Functions.R
+++ R/Functions.R
@@ -1,8 +1,8 @@
 
-#' scan.plot
+#' scan.seq
 #'
-#' Implement scan statistics (Kulldorff, 1997) for spatio-temporal data,
-#' and plot the result for each time point to detect hot-spots.
+#' Implement scan statistics (Kulldorff, 1997) for spatio-temporal data.
+#'
 #'
 #' @param data N (space) X M (time) matrix
 #' @param shp shape file with identical space order with data.
@@ -13,13 +13,12 @@
 #' @param alpha.level default is 0.05
 #' @param wd working directory for saving plots
 
-scan.plot <- function(data, shp, shp.name, centroid, pop.upper.bound = .2, n.simulations = 999, alpha.level = .05, wd) {
+scan.seq <- function(data, shp, shp.name, centroid, pop.upper.bound = .2, n.simulations = 999, alpha.level = .05, wd) {
 
   n.space <- nrow(shp)
 
   library(SpatialEpi)
   library(scanstatistics)
-  library(RColorBrewer)
 
   cent <- centroid
   geo <- SpatialEpi::latlong2grid(cent)
@@ -72,43 +71,76 @@ scan.plot <- function(data, shp, shp.name, centroid, pop.upper.bound = .2, n.sim
 
     if (length(cluster2)!=0) {
       id[[temp]] <- data.frame(week=rep(colnames(data)[i],length(cluster2)),
-                               sido=shp.name[cluster2, 1], sigungu=shp.name[cluster2, 2],
+                               state=shp.name[cluster2, 1], city=shp.name[cluster2, 2],
                                No_of_cases=No.cases, RR=RR, Exp.cases=Exp.cases, pvalue=p.val,
-                               total=total, cluster.cases=cluster.cases, percent=percent)
-      id[[temp]] <- id[[temp]][order(id[[temp]][,"sido"]),]
+                               total=total, cluster.cases=cluster.cases, percent=percent, shp.order=cluster2)
+      id[[temp]] <- id[[temp]][order(id[[temp]][,"state"]),]
     }
-
-    jpeg(paste("cluster_",colnames(data)[i],".jpeg",sep = ""), res=600, width=200, height=150, pointsize=9,units='mm')
-    brks <- cut(counts, breaks=c(0,210,490,1010,1600,max(counts)+1), include.lowest = TRUE, right=FALSE)
-    cols <- brewer.pal(5, "Reds")
-    for (k in 1:n.space) {col1[k] <- cols[brks[k]]}
-    plot(shp$geometry, col=col1,axes=F, border="gray")
-    plot(shp$geometry[cluster2], border="black", add=TRUE)
-    title(paste("Cluster map for", colnames(data)[i], sep=" "))
-    legend('bottomright',legend=c("[0, 210)", "[210, 490)", "[490, 1010)", "[1010, 1600)", "≥ 1600"),fill=cols,
-           cex=0.7)
-    dev.off()
 
     scan <- 0
 
     print(temp)
   }
-  capture.output(id, file="cluster info_covid_korea_by20221228.txt")
 
-  # for excel file
-  # tmp_df <- data.frame()
-  # for (i in 1:length(id)) {
-  #   tmp <- id[[i]]
-  #   if (!is.null(tmp)) {
-  #     tmp_df <- rbind(tmp_df, cbind(id = i, tmp))
-  #   }
-  # }
-  #
-  # tmp_df[order(tmp_df$week, -tmp_df$No_of_cases), ]
-  # tmp_table <- as.data.frame(table(tmp_df$id))
-  # tmp_df <- cbind(frequency = tmp_table$Freq[match(tmp_df$id, tmp_table$Var1)], tmp_df)
-  #
-  # library(xlsx)
-  # write.xlsx(tmp_df, '../result.xlsx', sheetName = 'entire', row.names = F, append = T)
+  capture.output(id, file="cluster info_covid_korea.txt")
+
+  # for csv file
+  tmp_df <- data.frame()
+  for (i in 1:length(id)) {
+    tmp <- id[[i]]
+    if (!is.null(tmp)) {
+      tmp_df <- rbind(tmp_df, cbind(id = i, tmp))
+    }
+  }
+
+  tmp_df[order(tmp_df$week, -tmp_df$No_of_cases), ]
+  tmp_table <- as.data.frame(table(tmp_df$id))
+  tmp_df <- cbind(frequency = tmp_table$Freq[match(tmp_df$id, tmp_table$Var1)], tmp_df)
+  write.csv(tmp_df, 'result.csv', row.names = F, fileEncoding = 'euc-kr')
+
+  return(id)
 }
 
+#' scan.plot
+#'
+#' Plot spatial hot-spot detected by scan statistics (Kulldorff, 1997).
+#'
+#' @param data N (space) X M (time) matrix
+#' @param shp shape file with identical space order with data.
+#' @param id list of cluster detected by scan.seq function
+#' @param wd working directory for saving plots
+
+scan.plot2 <- function(data, shp, id, wd) {
+
+  library(RColorBrewer)
+
+  if (is.null(id)) {
+    stop("id is null, so implement scan statistics using scan.seq function")
+  }
+
+  n.space <- nrow(shp)
+
+  setwd(wd)
+  col1 <- c(); temp <-0
+  for (i in 1:length(id)) {
+
+    temp <- temp + 1
+    index <- which(colnames(data) == unique(id[[i]]$week)) # in case of no cluster week
+    counts <- data[,index]
+    cluster2 <- id[[i]]$shp.order
+
+    jpeg(paste("cluster_",unique(id[[i]]$week),".jpeg",sep = ""), res=600, width=200, height=150, pointsize=9,units='mm')
+    brks <- cut(counts, breaks=c(0,210,490,1010,1600,max(counts)+1), include.lowest = TRUE, right=FALSE)
+    cols <- brewer.pal(5, "Reds")
+    for (k in 1:n.space) {col1[k] <- cols[brks[k]]}
+    plot(shp$geometry, col=col1,axes=F, border="gray")
+    plot(shp$geometry[cluster2], border="black", add=TRUE)
+    title(paste("Cluster map for", unique(id[[i]]$week), sep=" "))
+    legend('bottomright',legend=c("[0, 210)", "[210, 490)", "[490, 1010)", "[1010, 1600)", "≥ 1600"),fill=cols,
+           cex=0.7)
+    dev.off()
+
+    print(temp)
+  }
+
+}
