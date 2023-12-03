c1b2755418a650a8b45b091bab9ac84d11d246cb
Jaeseon Lee <jaeseon.lee@tamu.edu>
2023-12-03 02:48:45 -0600

    Added example datasets including covid_NY, shape files.

---
+++ R/data.R
@@ -0,0 +1,38 @@
+#' COVID-19 data
+#'
+#' A subset of data from
+#'
+#' @format ## 'covid_NY'
+#' A data frame with 338 rows and 31 columns:
+#' \describe{
+#'  \item{county}{county name}
+#'  \item{state}{state name}
+#' }
+#'
+"covid_NY"
+
+#' centroid
+#'
+#' A subset of data from
+#'
+#' @format ## 'centroid'
+#' A data frame with 338 rows and 2 columns:
+#' \describe{
+#'  \item{X}{longitude}
+#'  \item{Y}{latitude}
+#' }
+#'
+"centroid"
+
+#' shp_name
+#'
+#' A subset of data from
+#'
+#' @format ## 'shp_name'
+#' A data frame with 338 rows and 2 columns:
+#' \describe{
+#'  \item{state}{state name}
+#'  \item{county}{county name}
+#' }
+#'
+"shp_name"
